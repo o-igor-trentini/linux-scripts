4e4cddb890648e84809c0c8a36f92e32edfd44d1
Igor Trentini <igortrentini.2004@rastergr.com.br>
2023-06-26 11:55:34 -0300

    fix: Instalador spotify

--- spotify.sh
+++ spotify.sh
@@ -1,8 +1,7 @@
 # Adicionar repositório
-wget -O- https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
-
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 # Instalar
-sudo apt install spotify-client
+sudo apt-get update && sudo apt-get install spotify-client
 
